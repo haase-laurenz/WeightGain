a4b658d4bb427b26b16ac9c71f4c3a4a5c403e69
KingLauri200 <laurenz.haase@gmx.de>
2024-06-02 15:19:07 +0200

    added more features

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request, flash,session
+from flask import Flask, render_template, redirect, url_for, request, flash,session, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User
+from models import db, User, WeightEntry
 from flask_debugtoolbar import DebugToolbarExtension
 import time
 
@@ -31,6 +31,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/profile')
+@login_required
 def profile():
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
@@ -74,6 +75,47 @@ def market():
     users = User.query.all()
     return render_template('market.html', users=users)
 
+@app.route('/editProfile', methods=['GET', 'POST'])
+def editProfile():
+    if request.method == 'POST':
+        
+        current_user.username = request.form.get('username')
+        new_password = request.form.get('password')
+        
+        current_user.password = generate_password_hash(new_password, method='pbkdf2:sha256:600000')
+        db.session.commit()
+        
+        return redirect(url_for('profile'))
+        
+    return render_template('editProfile.html')
+
+@app.route('/weight_data', methods=['GET', 'POST'])
+def weight_data():
+    user = current_user
+    
+    if request.method == 'POST':
+        weight = request.form.get('weight')
+        new_entry = WeightEntry(weight=weight, user_id=user.id)
+        db.session.add(new_entry)
+        db.session.commit()        
+        
+    current_weight=""
+    if len(user.weight_entries)>0:
+        current_weight = str(user.weight_entries[-1].weight)+" kg."
+    else:
+        current_weight = "No weight data yet. Add now."
+        
+    return render_template('weight_data.html', current_weight=current_weight)
+
+@app.route('/weight_dataGraph')
+def weight_dataGraph():
+
+    user = current_user  # Beispiel für einen festgelegten Benutzer, ändern Sie dies entsprechend
+    entries = [
+        {'date': entry.date.isoformat(), 'weight': entry.weight}
+        for entry in user.weight_entries
+    ]
+    return jsonify(entries)
 
 if __name__ == '__main__':
     with app.app_context():
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
@@ -7,3 +8,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    budget = db.Column(db.Float, default=1000.0) 
+    weight_entries = db.relationship('WeightEntry', backref='user', lazy=True)
+    
+class WeightEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    weight = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
